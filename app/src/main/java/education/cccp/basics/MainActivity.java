37d752140b0af11a2822d46fb3fed84792c1fb8d
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-03-14 16:27:38 +0100

    On ajoute de resources strings avec internationalisation.

--- app/src/main/java/education/cccp/basics/MainActivity.java
+++ app/src/main/java/education/cccp/basics/MainActivity.java
@@ -1,10 +1,21 @@
 package education.cccp.basics;
 
-import android.annotation.SuppressLint;
+import static android.R.layout.simple_list_item_1;
+import static android.widget.LinearLayout.HORIZONTAL;
+import static android.widget.LinearLayout.VERTICAL;
+import static education.cccp.basics.R.drawable.pexels_pavel_danilyuk_8294792;
+import static education.cccp.basics.R.string.anglais;
+import static education.cccp.basics.R.string.célibataire;
+import static education.cccp.basics.R.string.français;
+import static education.cccp.basics.R.string.marié;
+import static education.cccp.basics.R.string.text1;
+import static education.cccp.basics.R.string.text2;
+
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -25,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
     Spinner spinner;
     ArrayAdapter adapter;
     String[] pays = {"france", "espagne", "italie"};
+    ImageView imageView;
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,27 +54,30 @@ public class MainActivity extends AppCompatActivity {
         cb2 = new CheckBox(this);
 
         radioGroup = new RadioGroup(this);
-        radioGroup.setOrientation(LinearLayout.HORIZONTAL);
+        radioGroup.setOrientation(HORIZONTAL);
 
         radioButton1 = new RadioButton(this);
-        radioButton1.setText("marié");
+        radioButton1.setText(marié);
 
         radioButton2 = new RadioButton(this);
-        radioButton2.setText("célibataire");
+        radioButton2.setText(célibataire);
 
         radioGroup.addView(radioButton1);
         radioGroup.addView(radioButton2);
 
+        imageView = new ImageView(this);
+        imageView.setImageResource(
+                pexels_pavel_danilyuk_8294792);
 
-        t1.setText("text 1");
-        t2.setText("text 2");
+        t1.setText(text1);
+        t2.setText(text2);
 
-        cb1.setText("Français");
-        cb2.setText("Anglais");
+        cb1.setText(français);
+        cb2.setText(anglais);
 
         spinner = new Spinner(this);
         adapter = new ArrayAdapter(this,
-                android.R.layout.simple_list_item_1,
+                simple_list_item_1,
                 pays);
 
         spinner.setAdapter(adapter);
@@ -74,8 +90,9 @@ public class MainActivity extends AppCompatActivity {
         l.addView(cb2);
         l.addView(radioGroup);
         l.addView(spinner);
+        l.addView(imageView);
 
-        l.setOrientation(LinearLayout.VERTICAL);
+        l.setOrientation(VERTICAL);
         setContentView(l);
 
 
